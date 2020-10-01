75fa117a53a4c47463fdd9561f66112745404e44
fabricio <fabricio.jacob@outlook.com>
2020-10-01 20:45:57 -0300

    add finished

--- Photos-Resource-Server-master/src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
+++ Photos-Resource-Server-master/src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
@@ -2,8 +2,10 @@ package com.appsdeveloperblog.ws.api.photos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotosApplication {
 
 	public static void main(String[] args) {
